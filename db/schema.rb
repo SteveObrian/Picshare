2b9eb4a66e69cf8523ebf793009ae78c47854414
SteveObrian <steve.krasel@posteo.de>
2018-06-20 15:42:03 +0200

    Adds Carrierwave to project and changes views for posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180620101226) do
+ActiveRecord::Schema.define(version: 20180620125749) do
 
   create_table "posts", force: :cascade do |t|
     t.string "caption"
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "image"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
