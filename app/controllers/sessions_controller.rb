11aebe7a604528fa4e81c5a8360eb46526191f8b
SteveObrian <steve.krasel@posteo.de>
2018-06-21 11:09:42 +0200

    Changes users index links

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
 
-      redirect_to :users, notice: 'Login Successful.'
+      redirect_to root_path, notice: 'Login Successful.'
     else
       flash.alert = 'Login failed.'
       render 'new'
